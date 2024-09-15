beaf59bd872b1ab691e79fb4d139f42ae2036e4d
codenameryuu <leaderballon@gmail.com>
2024-09-16 00:10:01 +0700

    Init all

---
+++ resources/views/about.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman about</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about" class="about">About</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1 class="judul">Tentang kami</h1>
+    <h2> Website di buat oleh: {{ $nama }}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus cum nihil, delectus sed ex
+        reiciendis cupiditate, nobis voluptatum sit sapiente dolorum odio sint ipsam facilis quae repudiandae. Neque,
+        et?</p>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about" class="about">About</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    <h1>Halaman kontak</h1>
+
+    <ul>
+        <li>
+            <a href="#">Instagram</a>
+        </li>
+        <li>
+            <a href="#">Facebook</a>
+        </li>
+        <li>
+            <a href="#">X</a>
+        </li>
+    </ul>
+</body>
+</html>
